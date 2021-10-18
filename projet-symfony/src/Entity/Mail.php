a6eeef62096c5e92a2f12b53af711329759984c3
Kévin Oliva <kevinoliva.pro@gmail.com>
2021-10-18 18:37:07 +0200

    Mise en place entité mail + envoi de mail + rectification vhosts

---
+++ projet-symfony/src/Entity/Mail.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MailRepository::class)
+ */
+class Mail
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sujet;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $corps;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $destinataire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->sujet;
+    }
+
+    public function setSujet(string $sujet): self
+    {
+        $this->sujet = $sujet;
+
+        return $this;
+    }
+
+    public function getCorps(): ?string
+    {
+        return $this->corps;
+    }
+
+    public function setCorps(string $corps): self
+    {
+        $this->corps = $corps;
+
+        return $this;
+    }
+
+    public function getDestinataire(): ?string
+    {
+        return $this->destinataire;
+    }
+
+    public function setDestinataire(string $destinataire): self
+    {
+        $this->destinataire = $destinataire;
+
+        return $this;
+    }
+}
